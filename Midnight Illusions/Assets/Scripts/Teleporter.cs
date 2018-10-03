54d1862f9a289adb7cc3aa97dfaafe1cb9cc4486
Jose Solano <jsolanom@ucenfotec.ac.cr>
2018-10-03 14:38:11 -0600

    Master Project Basics (Heiner Morales)
    Created by Heiner Morales

---
+++ Midnight Illusions/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform rayTransform;
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One) || Input.GetMouseButtonDown(0) || Input.GetButtonDown("Fire1"))
+        {
+            var ray = new Ray(rayTransform.position, rayTransform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 50))
+            {
+                if (hit.collider.tag == "Teleport")
+                {
+                    Transform destination = hit.collider.transform.Find("Destination");
+                    rayTransform.parent.parent.position = new Vector3(destination.position.x, rayTransform.parent.parent.position.y, destination.position.z);
+                }
+            }
+        }
+    }
+}
