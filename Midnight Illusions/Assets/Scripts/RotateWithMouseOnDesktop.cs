2220bbd170f162c0baf9b53b1826349fa883578e
Heiner Morales <hmorales@outcoding.com>
2018-10-03 23:05:25 -0600

    Sun, Night Skybox, Editing map, placing teleporters

---
+++ Midnight Illusions/Assets/Scripts/RotateWithMouseOnDesktop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class RotateWithMouseOnDesktop : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    private void Start()
+    {
+        if (!SystemInfo.operatingSystem.Contains("Windows"))
+            enabled = false;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+}
